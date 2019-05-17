dabe1e6528de8ef611a8f02eb9c5141b222de652
Om Prakash C <comprakash@gmail.com>
2019-05-17 15:12:23 +0530

    Added Search box component

--- myapp01/src/App.js
+++ myapp01/src/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import CommentBox from "./components/comment-box/comment-box";
-import User from "./components/user/user";
-import Menu from "./components/menu/menu";
-import Myuser from "./data/my-user";
-import MyTextBox from "./components/my-text-box/my-text-box";
-import MyButton from "./components/my-button/my-button";
-import MyLine from "./components/my-line/my-line";
-import UserGist from "./components/user-gist/user-gist";
+import SearchBox from "./components/search-box/search-box";
+// import CommentBox from "./components/comment-box/comment-box";
+// import User from "./components/user/user";
+// import Menu from "./components/menu/menu";
+// import Myuser from "./data/my-user";
+// import MyTextBox from "./components/my-text-box/my-text-box";
+// import MyButton from "./components/my-button/my-button";
+// import MyLine from "./components/my-line/my-line";
+// import UserGist from "./components/user-gist/user-gist";
+// import MyMap from "./components/my-map/my-map";
+// import RepoList from "./components/repo-list/repo-list";
+// import $ from 'jquery';
 
 class App extends Component {
   render() {
@@ -16,13 +20,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <CommentBox />
+          {/* <CommentBox />
           <User user={Myuser}/>
           <Menu />
           <MyTextBox />
           <MyButton />
           <MyLine name="fades away ... and springs back!!"/>
           <UserGist source="https://api.github.com/users/octocat/gists" />
+          <MyMap />
+          <RepoList prm={$.getJSON('https://api.github.com/search/repositories?q=javascript&sort=stars')} /> */}
+          <SearchBox />
         </header>
       </div>
     );
