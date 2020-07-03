61349c102b7eb0201ce959e4231dc6855f9b3584
Om.Prakash <om.prakash@sony.com>
2020-07-03 14:51:36 +0530

    Added state in React

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -11,12 +11,17 @@ const lakeList = [
 ]
 
 function App({lakes}) {
+  const [status, setStatus] = useState("Open");
   return (
     <>
       <h1>India's Biggest Lakes</h1>
       <ul>
         {lakes.map((lake) => <li key={lake}>{lake}</li>)}
       </ul>
+      <h1>App Status: {status}</h1>
+      <button onClick={() => setStatus("Open")}>Open</button>
+      <button onClick={() => setStatus("Back in 5")}>Break</button>
+      <button onClick={() => setStatus("Closed")}>Closed</button>
     </>
   );
 }
